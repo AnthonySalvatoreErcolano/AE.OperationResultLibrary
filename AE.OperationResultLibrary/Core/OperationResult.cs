12e3e3678b76bc92892bf1fb31b9c23ca579748a
Anthony Ercolano <aercolano1123@outlook.com>
2025-12-15 12:34:10 -0500

    Add project files.

---
+++ AE.OperationResultLibrary/Core/OperationResult.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AE.OperationResultLibrary.Core
+{
+    public  class OperationResult
+    {
+        public string ResponseText { get; set; } = string.Empty;
+        public ResponseValue Value { get; set; } = ResponseValue.Failed;
+
+        public OperationResult(string message, ResponseValue responseValue)
+        {
+            Value = responseValue;
+            ResponseText = message;
+        }
+
+        public OperationResult()
+        {
+            
+        }
+    }
+
+    public class OperationResult<T> : OperationResult
+    {
+        public T? Item1 { get; set; }
+
+        public OperationResult(T data, string message, ResponseValue responseValue) :base (message, responseValue)
+        {
+            Item1= data;    
+        }
+
+        public static OperationResult<T> Success(T data, string message = "Operation completed successfully.")
+        {
+            return new OperationResult<T>(data, message, ResponseValue.Success);
+        }
+    }
+
+    public class OperationResult<T1,T2> : OperationResult
+    {
+        public T1? Item1 { get; set; }
+
+        public T2? Item2 { get; set; }
+    }
+
+    public class OperationResult<T1, T2,T3> : OperationResult
+    {
+        public T1? Item1 { get; set; }
+
+        public T2? Item2 { get; set; }
+
+        public T3? Item3 { get; set; }
+    }
+}
---
+++ AE.OperationResultLibrary/Core/ResponseValue.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AE.OperationResultLibrary.Core
+{
+    public class ResponseValue
+    {
+        public int Code { get;}
+        public string Name { get; }
+
+        private ResponseValue(int code, string name)
+        {
+            Code = code;
+            Name = name;
+
+        }
+
+        public static bool operator ==(ResponseValue a, ResponseValue b) => a.Code == b.Code;
+        public static bool operator !=(ResponseValue a, ResponseValue b) => a.Code != b.Code;
+        public override string ToString() => Name;
+
+        public static ResponseValue Success { get; } = new ResponseValue(200, nameof(Success));
+        public static ResponseValue NotFound { get; } = new ResponseValue(404, nameof(NotFound));
+        public static ResponseValue Invalid { get; } = new ResponseValue(400, nameof(Invalid));
+        public static ResponseValue Failed { get; } = new ResponseValue(500, nameof(Failed));
+        public static ResponseValue Unauthorized { get; } = new ResponseValue(401, nameof(Unauthorized));
+        public static ResponseValue Forbidden { get; } = new ResponseValue(403, nameof(Forbidden));
+        public static ResponseValue Warning { get; } = new ResponseValue(202, nameof(Warning));
+
+
+        /// <summary>
+        /// Using the C# 14 extension blocks, you can define custom response values. 
+        /// </summary>
+        /// <param name="code"></param>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public static ResponseValue DefineCustom(int code, string name) => new ResponseValue(code, name);
+     
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is null)
+                return false;
+            if (obj is ResponseValue other)
+            {
+                return Code == other.Code; 
+            }
+            return false;
+        }
+
+        public override int GetHashCode() =>  HashCode.Combine(Code, Name);
+        
+
+    }
+}
